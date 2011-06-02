6b587a356e6ad94e3b66eee1c3cfc58ba774ec9d
Richard Beare <Richard.Beare@gmail.com>
2011-06-02 17:11:50 +1000

    first pass at intermodes

--- testTriangle.cxx
+++ testTriangle.cxx
@@ -28,20 +28,20 @@ int main(int argc, char * argv[])
 
   RawImType::Pointer raw = readIm<RawImType>(argv[1]);
 
-  itk::Instance <itk::OtsuThresholdImageFilter<RawImType, LabImType > > Otsu;
-  Otsu->SetInput(raw);
-  Otsu->SetOutsideValue(1);
-  Otsu->SetInsideValue(0);
+  // itk::Instance <itk::OtsuThresholdImageFilter<RawImType, LabImType > > Otsu;
+  // Otsu->SetInput(raw);
+  // Otsu->SetOutsideValue(1);
+  // Otsu->SetInsideValue(0);
 
-  writeIm<LabImType>(Otsu->GetOutput(), argv[2]);
-  std::cout << "Otsu threshold: " << (float)Otsu->GetThreshold() << std::endl;
+  // writeIm<LabImType>(Otsu->GetOutput(), argv[2]);
+  // std::cout << "Otsu threshold: " << (float)Otsu->GetThreshold() << std::endl;
   
   itk::Instance <itk::TriangleThresholdImageFilter<RawImType, LabImType > > Tri;
   Tri->SetInput(raw);
   Tri->SetOutsideValue(1);
   Tri->SetInsideValue(0);
 
-  writeIm<LabImType>(Tri->GetOutput(), argv[3]);
+  writeIm<LabImType>(Tri->GetOutput(), argv[2]);
   std::cout << "Triangle threshold: " << (float)Tri->GetThreshold() << std::endl;
   
   return(EXIT_SUCCESS);
